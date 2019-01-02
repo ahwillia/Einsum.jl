bb3233b80c1eade0a13892ad7f1dcfb17c472c5c
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-01-02 17:39:20 +0100

    Improve a lot of variable names for better readability and consistency

--- src/Einsum.jl
+++ src/Einsum.jl
@@ -29,129 +29,129 @@ macro einsum_checkinbounds(ex)
 end
 
 
-function _einsum(ex::Expr, inbounds = true, simd = false, threads = false)
+function _einsum(expr::Expr, inbounds = true, simd = false, threads = false)
     # Get left hand side (lhs) and right hand side (rhs) of equation
-    lhs = ex.args[1]
-    rhs = ex.args[2]
+    lhs = expr.args[1]
+    rhs = expr.args[2]
 
     # Get info on the left-hand side
-    lhs_idx, lhs_arr, lhs_dim = extractindices(lhs)
-    length(lhs_arr) != 1 && throw(ArgumentError(
-        "Left-hand side of equation contains multiple arguments. Only a single referencing ",
-        " expression (e.g. @einsum A[i] = ...) should be used."))
+    lhs_arrays, lhs_indices, lhs_axis_exprs = extractindices(lhs)
+    length(lhs_arrays) != 1 && throw(ArgumentError(string(
+        "Left-hand side of equation contains multiple arguments. Only a single ",
+        "referencing expression (e.g. @einsum A[i] = ...) should be used.")))
 
     # Get info on the right-hand side
-    rhs_idx, rhs_arr, rhs_dim = extractindices(rhs)
+    rhs_arrays, rhs_indices, rhs_axis_exprs = extractindices(rhs)
 
     # remove duplicate indices on left-hand and right-hand side
     # and ensure that the array sizes match along these dimensions
     ###########################################################
-    ex_check_dims = Expr[]
+    dimension_checks = Expr[]
 
     # remove duplicate indices on the right hand side
-    for i in reverse(eachindex(rhs_idx))
+    for i in reverse(eachindex(rhs_indices))
         duplicated = false
-        di = rhs_dim[i]
+        di = rhs_axis_exprs[i]
         
         for j = 1:(i - 1)
-            if rhs_idx[j] == rhs_idx[i]
+            if rhs_indices[j] == rhs_indices[i]
                 # found a duplicate
                 duplicated = true
-                dj = rhs_dim[j]
+                dj = rhs_axis_exprs[j]
 
                 # add dimension check ensuring consistency
-                push!(ex_check_dims, :(@assert $dj == $di))
+                push!(dimension_checks, :(@assert $dj == $di))
             end
         end
         
-        for j = eachindex(lhs_idx)
-            if lhs_idx[j] == rhs_idx[i]
-                dj = lhs_dim[j]
-                if Meta.isexpr(ex, :(:=))
-                    # ex.head is :=
+        for j = eachindex(lhs_indices)
+            if lhs_indices[j] == rhs_indices[i]
+                dj = lhs_axis_exprs[j]
+                if Meta.isexpr(expr, :(:=))
+                    # expr.head is :=
                     # infer the size of the lhs array
-                    lhs_dim[j] = di
+                    lhs_axis_exprs[j] = di
                 else
-                    # ex.head is =, +=, *=, etc.
-                    lhs_dim[j] = :(min($dj, $di))
+                    # expr.head is =, +=, *=, etc.
+                    lhs_axis_exprs[j] = :(min($dj, $di))
                 end
                 duplicated = true
             end
         end
         
         if duplicated
-            deleteat!(rhs_idx, i)
-            deleteat!(rhs_dim, i)
+            deleteat!(rhs_indices, i)
+            deleteat!(rhs_axis_exprs, i)
         end
     end
 
     # remove duplicate indices on the left hand side
-    for i in reverse(eachindex(lhs_idx))
+    for i in reverse(eachindex(lhs_indices))
         duplicated = false
-        di = lhs_dim[i]
+        di = lhs_axis_exprs[i]
 
         # don't need to check rhs, already done above
 
         for j = 1:(i - 1)
-            if lhs_idx[j] == lhs_idx[i]
+            if lhs_indices[j] == lhs_indices[i]
                 # found a duplicate
                 duplicated = true
-                dj = lhs_dim[j]
+                dj = lhs_axis_exprs[j]
 
                 # add dimension check
-                push!(ex_check_dims, :(@assert $dj == $di))
+                push!(dimension_checks, :(@assert $dj == $di))
             end
         end
         
         if duplicated
-            deleteat!(lhs_idx, i)
-            deleteat!(lhs_dim, i)
+            deleteat!(lhs_indices, i)
+            deleteat!(lhs_axis_exprs, i)
         end
     end
 
     # Create output array if specified by user
     @gensym T
 
-    if Meta.isexpr(ex, :(:=))
+    if Meta.isexpr(expr, :(:=))
         # infer type of allocated array
-        #    e.g. rhs_arr = [:A, :B]
+        #    e.g. rhs_arrays = [:A, :B]
         #    then the following line produces :(promote_type(eltype(A), eltype(B)))
-        rhs_type = :(promote_type($([:(eltype($arr)) for arr in rhs_arr]...)))
+        rhs_type = :(promote_type($([:(eltype($arr)) for arr in rhs_arrays]...)))
 
-        ex_get_type = :(local $T = $rhs_type)
+        type_definition = :(local $T = $rhs_type)
 
-        ex_create_arrays = if length(lhs_dim) > 0
-            :($(lhs_arr[1]) = Array{$rhs_type}(undef, $(lhs_dim...)))
+        output_definition = if length(lhs_axis_exprs) > 0
+            :($(lhs_arrays[1]) = Array{$rhs_type}(undef, $(lhs_axis_exprs...)))
         else
-            :($(lhs_arr[1]) = zero($rhs_type))
+            :($(lhs_arrays[1]) = zero($rhs_type))
         end
 
-        ex_assignment_op = :(=)
+        assignment_op = :(=)
     else
-        ex_get_type = :(local $T = eltype($(lhs_arr[1])))
-        ex_create_arrays = :(nothing)
-        ex_assignment_op = ex.head
+        type_definition = :(local $T = eltype($(lhs_arrays[1])))
+        output_definition = :(nothing)
+        assignment_op = expr.head
     end
 
-    if threads && !Meta.isexpr(ex, :(=)) && !Meta.isexpr(ex, :(:=))
-        throw(ArgumentError(
-            string("Threaded @vielsum can only assign with = or := right now. ",
-                   "To use ", ex.head, " try @einsum instead.")))
+    if threads && !Meta.isexpr(expr, :(=)) && !Meta.isexpr(expr, :(:=))
+        throw(ArgumentError(string(
+            "Threaded @vielsum can only assign with = or := right now. ",
+            "To use ", expr.head, " try @einsum instead.")))
         # could allow :(+=) by simply then removing $lhs = zero($T) line
     end
 
-    if threads && length(lhs_idx) == 0
+    if threads && length(lhs_indices) == 0
         # this won't actually cause problems, but won't use threads
-        throw(ArgumentError(
-            string("Threaded @vielsum can't assign to a scalar LHS. ",
-                   "Try @einsum instead.")))
+        throw(ArgumentError(string(
+            "Threaded @vielsum can't assign to a scalar LHS. ",
+            "Try @einsum instead.")))
     end
 
-    # Copy equation, ex is the Expr we'll build up and return.
-    unquote_offsets!(ex)
+    # Copy equation, expr is the Expr we'll build up and return.
+    unquote_offsets!(expr)
 
     # Next loops to iterate over the destination variables
-    if length(rhs_idx) > 0
+    if length(rhs_indices) > 0
         # There are indices on rhs that do not appear in lhs.
         # We sum over these variables.
 
@@ -159,55 +159,55 @@ function _einsum(ex::Expr, inbounds = true, simd = false, threads = false)
 
             # Innermost expression has form s += rhs
             @gensym s
-            ex.args[1] = s
-            ex.head = :(+=)
+            expr.args[1] = s
+            expr.head = :(+=)
 
             # Nest loops to iterate over the summed out variables
-            ex = nest_loops(ex, rhs_idx, rhs_dim, simd, false)
+            expr = nest_loops(expr, rhs_indices, rhs_axis_exprs, simd, false)
 
             # Prepend with s = 0, and append with assignment
             # to the left hand side of the equation.
-            lhs_assignment = Expr(ex_assignment_op, lhs, s)
+            lhs_assignment = Expr(assignment_op, lhs, s)
 
-            ex = quote
+            expr = quote
                 local $s = zero($T)
-                $ex
+                $expr
                 $lhs_assignment
             end
 
         else # we are threading, and thus should write directly to lhs array
 
-            ex.args[1] = lhs
-            ex.head = :(+=)
+            expr.args[1] = lhs
+            expr.head = :(+=)
 
-            ex = nest_loops(ex, rhs_idx, rhs_dim, simd, false)
+            expr = nest_loops(expr, rhs_indices, rhs_axis_exprs, simd, false)
 
-            ex = quote
+            expr = quote
                 $lhs = zero($T)
-                $ex
+                $expr
             end
         end
 
         # Now loop over indices appearing on lhs, if any
-        ex = nest_loops(ex, lhs_idx, lhs_dim, false, threads)
+        expr = nest_loops(expr, lhs_indices, lhs_axis_exprs, false, threads)
     else
         # We do not sum over any indices, only loop over lhs
-        ex.head = ex_assignment_op
-        ex = nest_loops(ex, lhs_idx, lhs_dim, simd, threads)
+        expr.head = assignment_op
+        expr = nest_loops(expr, lhs_indices, lhs_axis_exprs, simd, threads)
     end
 
     if inbounds
-        ex = :(@inbounds $ex)
+        expr = :(@inbounds $expr)
     end
 
     full_expression = quote
-        $ex_create_arrays
+        $output_definition
         let
-            $(ex_check_dims...)
-            $ex_get_type
-            $ex
+            $(dimension_checks...)
+            $type_definition
+            $expr
         end
-        $(lhs_arr[1])
+        $(lhs_arrays[1])
     end
 
     return esc(full_expression)
@@ -215,9 +215,9 @@ end
 
 
 """
-    nest_loops(expr, idx, dim, simd, threads) -> Expr
+    nest_loops(expr, indices, axis_exprs, simd, threads) -> Expr
 
-Construct a nested loop around `expr`, using indices `idx` in ranges `dim`.
+Construct a nested loop around `expr`, using `indices` in ranges `axis_exprs`.
 
 # Example
 ```julia-repl
@@ -230,24 +230,29 @@ quote
 end
 ```
 """
-function nest_loops(ex::Expr, idx::Vector{Symbol}, dim::Vector{Expr}, simd::Bool, threads::Bool)
-    isempty(idx) && return ex
+function nest_loops(expr::Expr,
+                    index_names::Vector{Symbol}, axis_expressions::Vector{Expr},
+                    simd::Bool, threads::Bool)
+    isempty(index_names) && return expr
 
     # Add @simd to the innermost loop, if required
     # and @threads to the outermost loop
-    ex = nest_loop(ex, idx[1], dim[1], simd, threads && length(idx) == 1)
+    expr = nest_loop(expr, index_names[1], axis_expressions[1],
+                     simd, threads && length(index_names) == 1)
 
     # Add remaining for loops
-    for j = 2:length(idx)
-        ex = nest_loop(ex, idx[j], dim[j], false, threads && length(idx) == j)
+    for j = 2:length(index_names)
+        expr = nest_loop(expr, index_names[j], axis_expressions[j],
+                         false, threads && length(index_names) == j)
     end
 
-    return ex
+    return expr
 end
 
-function nest_loop(ex::Expr, ix::Symbol, dim::Expr, simd::Bool, threads::Bool)
-    loop = :(for $ix = 1:$dim
-                 $ex
+function nest_loop(expr::Expr, index_name::Symbol, axis_expression::Expr,
+                   simd::Bool, threads::Bool)
+    loop = :(for $index_name = 1:$axis_expression
+                 $expr
              end)
 
     if threads
@@ -257,141 +262,151 @@ function nest_loop(ex::Expr, ix::Symbol, dim::Expr, simd::Bool, threads::Bool)
     end
 
     return quote
-        local $ix
+        local $index_name
         $loop
     end
 end
 
 
 """
-    extractindices(expr) -> (indices, arrays, dims)
+    extractindices(expr) -> (array_names, index_names, axis_expressions)
 
-Compute lists of all `indices` and `dims` of an expression of `arrays`.  Everything is ordered by
-first occurence in `expr`.
+Compute all `index_names` and respective axis calculations of an expression 
+involving the arrays with `array_names`. Everything is ordered by first 
+occurence in `expr`.
 
 # Examples
 ```julia-repl
 julia> extractindices(:(f(A[i,j,i]) + C[j]))
-(Symbol[:i, :j, :i, :j], Symbol[:A, :C], Expr[:(size(A, 1)), :(size(A, 2)), :(size(A, 3)), :(size(C, 1))])
+(Symbol[:A, :C], Symbol[:i, :j, :i, :j], Expr[:(size(A, 1)), :(size(A, 2)), :(size(A, 3)), :(size(C, 1))])
 ```
 """
-extractindices(ex) = extractindices!(ex, Symbol[], Symbol[], Expr[])
+extractindices(expr) = extractindices!(expr, Symbol[], Symbol[], Expr[])
 
-function extractindices!(ex::Symbol,
-                         idx_store::Vector{Symbol},
-                         arr_store::Vector{Symbol},
-                         dim_store::Vector{Expr})
-    push!(arr_store, ex)
-    return idx_store, arr_store, dim_store
+function extractindices!(expr::Symbol,
+                         array_names::Vector{Symbol},
+                         index_names::Vector{Symbol},
+                         axis_expressions::Vector{Expr})
+    push!(array_names, expr)
+    return array_names, index_names, axis_expressions
 end
 
-function extractindices!(ex::Number,
-                         idx_store::Vector{Symbol},
-                         arr_store::Vector{Symbol},
-                         dim_store::Vector{Expr})
-    return idx_store, arr_store, dim_store
+function extractindices!(expr::Number,
+                         array_names::Vector{Symbol},
+                         index_names::Vector{Symbol},
+                         axis_expressions::Vector{Expr})
+    return array_names, index_names, axis_expressions
 end
 
 
-function extractindices!(ex::Expr,
-                         idx_store::Vector{Symbol},
-                         arr_store::Vector{Symbol},
-                         dim_store::Vector{Expr})
-    if Meta.isexpr(ex, :ref) # e.g. A[i,j,k]
-        arrname = ex.args[1]
-        push!(arr_store, arrname)
+function extractindices!(expr::Expr,
+                         array_names::Vector{Symbol},
+                         index_names::Vector{Symbol},
+                         axis_expressions::Vector{Expr})
+    if Meta.isexpr(expr, :ref) # e.g. A[i,j,k]
+        array_name = expr.args[1]
+        push!(array_names, array_name)
 
-        # ex.args[2:end] are indices (e.g. [i,j,k])
-        for (pos, idx) in enumerate(ex.args[2:end])
-            extractindex!(idx, arrname, pos, idx_store, arr_store, dim_store)
+        # expr.args[2:end] are indices (e.g. [i,j,k])
+        for (dimension, index_expr) in enumerate(expr.args[2:end])
+            extractindex!(index_expr, array_name, dimension,
+                          array_names, index_names, axis_expressions)
         end
-    elseif Meta.isexpr(ex, :call)
+    elseif Meta.isexpr(expr, :call)
         # e.g. 2*A[i,j], transpose(A[i,j]), or A[i] + B[j], so
-        # ex.args[2:end] are the individual tensor expressions (e.g. [A[i], B[j]])
-        for arg in ex.args[2:end]
-            extractindices!(arg, idx_store, arr_store, dim_store)
+        # expr.args[2:end] recursively contain the individual tensor
+        # expressions (e.g. [A[i], B[j]])
+        for arg in expr.args[2:end]
+            extractindices!(arg, array_names, index_names, axis_expressions)
         end
     else
-        throw(ArgumentError("Invalid expression head: `:$(ex.head)`"))
+        throw(ArgumentError("Invalid expression head: `:$(expr.head)`"))
     end
 
-    return idx_store, arr_store, dim_store
+    return return array_names, index_names, axis_expressions
 end
 
-function extractindex!(ex::Symbol, arrname, position,
-                       idx_store, arr_store, dim_store)
-    push!(idx_store, ex)
-    push!(dim_store, :(size($arrname, $position)))
-    return idx_store, arr_store, dim_store
+function extractindex!(expr::Symbol, array_name::Symbol, dimension::Int,
+                       array_names::Vector{Symbol},
+                       index_names::Vector{Symbol},
+                       axis_expressions::Vector{Expr})
+    push!(index_names, expr)
+    push!(axis_expressions, :(size($array_name, $dimension)))
+    return array_names, index_names, axis_expressions
 end
 
-function extractindex!(ex::Number, arrname, position,
-                       idx_store, arr_store, dim_store)
-    return idx_store, arr_store, dim_store
+function extractindex!(expr::Number, array_name::Symbol, dimension::Int,
+                       array_names::Vector{Symbol},
+                       index_names::Vector{Symbol},
+                       axis_expressions::Vector{Expr})
+    return array_names, index_names, axis_expressions
 end
 
-function extractindex!(ex::Expr, arrname, position,
-                       idx_store, arr_store, dim_store)
+function extractindex!(expr::Expr, array_name::Symbol, dimension::Int,
+                       array_names::Vector{Symbol},
+                       index_names::Vector{Symbol},
+                       axis_expressions::Vector{Expr})
     # e.g. A[i+:offset] or A[i+5]
-    #    ex is an Expr in this case
+    #    expr is an Expr in this case
     #    We restrict it to be a Symbol (e.g. :i) followed by either
     #        a number or quoted expression.
     #    As before, push :i to index list
     #    Need to add/subtract off the offset to dimension list
 
-    if Meta.isexpr(ex, :call) && length(ex.args) == 3
-        op = ex.args[1]
+    if Meta.isexpr(expr, :call) && length(expr.args) == 3
+        op = expr.args[1]
 
-        idx = ex.args[2]
-        @assert typeof(idx) == Symbol
+        index_name = expr.args[2]
+        @assert typeof(index_name) == Symbol
 
-        off_expr = ex.args[3]
+        offset_expr = expr.args[3]
 
-        if off_expr isa Integer
-            off = ex.args[3]::Integer
-        elseif off_expr isa Expr && Meta.isexpr(off_expr, :quote)
-            off = off_expr.args[1]
-        elseif off_expr isa QuoteNode
-            off = off_expr.value
+        if offset_expr isa Integer
+            offset = expr.args[3]::Integer
+        elseif offset_expr isa Expr && Meta.isexpr(offset_expr, :quote)
+            offset = offset_expr.args[1]
+        elseif offset_expr isa QuoteNode
+            offset = offset_expr.value
         else
             throw(ArgumentError("Improper expression inside reference on rhs"))
         end
 
         # push :i to indices we're iterating over
-        push!(idx_store, idx)
+        push!(index_names, index_name)
 
         # need to invert + or - to determine iteration range
         if op == :+
-            push!(dim_store, :(size($arrname, $position) - $off))
+            push!(axis_expressions, :(size($array_name, $dimension) - $offset))
         elseif op == :-
-            push!(dim_store, :(size($arrname, $position) + $off))
+            push!(axis_expressions, :(size($array_name, $dimension) + $offset))
         else
-            throw(ArgumentError("Operations inside ref on rhs are limited to `+` or `-`"))
+            throw(ArgumentError(string("Operations inside ref on rhs are ",
+                                       "limited to `+` and `-`")))
         end
-    elseif Meta.isexpr(ex, :quote)
+    elseif Meta.isexpr(expr, :quote)
         # nothing
     else
-        throw(ArgumentError("Invalid index expression: `$(ex)`"))
+        throw(ArgumentError("Invalid index expression: `$(expr)`"))
     end
 
-    return idx_store, arr_store, dim_store
+    return array_names, index_names, axis_expressions
 end
 
 
-function unquote_offsets!(ex::Expr, inside_ref = false)
-    inside_ref |= Meta.isexpr(ex, :ref)
+function unquote_offsets!(expr::Expr, inside_ref = false)
+    inside_ref |= Meta.isexpr(expr, :ref)
     
-    for i in eachindex(ex.args)
-        if ex.args[i] isa Expr
-            if Meta.isexpr(ex.args[i], :quote) && inside_ref # never seems to get here
-                ex.args[i] = ex.args[i].args[1]
+    for i in eachindex(expr.args)
+        if expr.args[i] isa Expr
+            if Meta.isexpr(expr.args[i], :quote) && inside_ref # never seems to get here
+                expr.args[i] = expr.args[i].args[1]
             else
-                unquote_offsets!(ex.args[i], inside_ref)
+                unquote_offsets!(expr.args[i], inside_ref)
             end
         end
     end
 
-    return ex
+    return expr
 end
 
 # end module
